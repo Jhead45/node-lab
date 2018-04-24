7fab6924b945534f21170ddddd4c4828cb3324f3
Justin Head <justin_head@ymail.com>
2018-04-24 09:35:11 -0500

    changed to createWriteStream

--- popular-downloader.js
+++ popular-downloader.js
@@ -2,16 +2,16 @@ const path = require('path');
 const fs = require('fs');
 const rp = require('request-promise');
 
-const dataPath = path.join(__dirname, 'downloads');
+const dataPath = path.join(__dirname, './downloads');
 
 rp('https://reddit.com/r/popular.json')
     .then((res) => {
         let resParsed = JSON.parse(res);
         let articles = resParsed.data.children.map(item => {
             if (path.extname(item.data.url) === '.jpg' || path.extname(item.data.url) === '.png' || path.extname(item.data.url) === '.gifv'){
-                fs.writeFile((`${dataPath}/${item.data.title}`), item.data.url, (err) => {
-                  if (err)  console.log(err);   
-               })
+               
+                rp(item.data.url).pipe(fs.createWriteStream(`./downloads/${item.data.id}${path.extname(item.data.url)}`));
+               
             }
         });                
         
